a0638e3c1f8af85d162bfa2f194b53579d301865
Parker Bartlett <prbartlett1@gmail.com>
2019-03-14 10:37:18 -0400

    fixed footer and corrected carousel images

--- js/app.js
+++ js/app.js
@@ -1,23 +1,41 @@
 const icon_div = document.querySelector('.icon');
 const mobileNav_div = document.querySelector('.mobile-nav')
+const prev_a = document.querySelector('.prev')
+const next_a = document.querySelector('.next')
+const dotOne_span = document.querySelector('.dot-one')
+const dotTwo_span = document.querySelector('.dot-two')
+const dotThree_span = document.querySelector('.dot-three')
+
+
+
 
 function toggleIcon() {
-    icon_div.classList.toggle('change')
+  icon_div.classList.toggle('change')
 }
 
 function showMobileNav() {
-    mobileNav_div.classList.toggle('show-mobile-nav')
+  mobileNav_div.classList.toggle('show-mobile-nav')
 }
 
 function eventListener() {
-    icon_div.addEventListener('click', toggleIcon);
-    icon_div.addEventListener('click', showMobileNav);
+  icon_div.addEventListener('click', toggleIcon);
+  icon_div.addEventListener('click', showMobileNav);
 }
 
 eventListener()
 
 
 
+
+
+// prev_a.addEventListener('click', plusSlides(-1))
+// next_a.addEventListener('click', plusSlides(1))
+
+// dotOne_span.addEventListener('click', currentSlide(1))
+// dotTwo_span.addEventListener('click', currentSlide(2))
+// dotThree_span.addEventListener('click', currentSlide(3))
+
+   
 
 var slideIndex = 1;
 showSlides(slideIndex);
@@ -36,16 +54,16 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
 
 
@@ -53,5 +71,5 @@ function showSlides(n) {
 
 
 module.exports = {
-    toggleIcon: toggleIcon
+  toggleIcon: toggleIcon
 }
